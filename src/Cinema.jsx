a6e0df5aa0e7d307b888fa901f513acab426380c
Andre Carpio <carpioandre7@gmail.com>
2025-06-05 14:55:14 -0400

    rows and columns

---
+++ src/Cinema.jsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import "./Cinema.css";
+import { SeatIcon } from "./SeatIcon";
+
+export const Cinema = ({ rows = 5, numSeats = 10 }) => {
+  const [seats, setSeats] = useState(createSeats());
+
+  function createSeats() {
+    const newSeats = [];
+    for (let i = 0; i < rows; i++) {
+      const row = [];
+      for (let j = 0; j < numSeats; j++) {
+        row.push(false);
+      }
+      newSeats.push(row);
+    }
+    return newSeats;
+  }
+
+  const reserveSeat = (seat, indexRow, indexColum) => {
+    if (seat) {
+      alert("This seat has already been reserved.");
+    }
+    setSeats((prev) => {
+      let newSeats = structuredClone(prev);
+      newSeats[indexRow][indexColum] = true;
+      return newSeats;
+    });
+  };
+
+  return (
+    <>
+      <div className="cinema">
+        {seats.map((row, indexRow) => {
+          return (
+            <div key={indexRow} className="row">
+              {row.map((seat, indexColum) => {
+                return (
+                  <div
+                    key={` ${indexRow}-${indexColum}`}
+                    className="seat"
+                    onClick={() => {
+                      reserveSeat(seat, indexRow, indexColum);
+                    }}
+                  >
+                    <SeatIcon fill={seat ? "red" : "#00e0e0"}></SeatIcon>
+                  </div>
+                );
+              })}
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+};
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { App } from './App.jsx'
+import { Cinema } from './Cinema'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+   <Cinema  ></Cinema>
   </StrictMode>,
 )
