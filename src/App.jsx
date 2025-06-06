d57ddbcebd9fb919f958de994c5f2986d8b78d0c
Andre Carpio <carpioandre7@gmail.com>
2025-06-06 12:19:22 -0400

    creating boxes and styles

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+export const App = () => {
+  return (
+    <>
+      <div class="c1"></div>
+      <div class="c2"></div>
+      <div class="c3"></div>
+      <div class="c4">
+        <div class="c4_1"></div>
+        <div class="c4_2"></div>
+      </div>
+    </>
+  );
+};
