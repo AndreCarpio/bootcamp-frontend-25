854ee424d6e2fea508220cf6f2f32c52424315ca
AndreCarpio <Andre.Carpio@jala.university>
2025-06-12 17:03:45 -0400

    search method

--- src/components/molecules/Weather.jsx
+++ src/components/molecules/Weather.jsx
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Weather.css";
 
 export const Weather = () => {
+  const [searchInput, setSearchInput] = useState("");
+  const [weather, setWeather] = useState(null);
+
+  function search(name) {}
+
   return (
     <>
       <h1>Weather app</h1>
 
-      <input type="text" name="" id="" />
+      <input
+        type="text"
+        value={searchInput}
+        onChange={(e) => setSearchInput(e.value)}
+      />
       <button>Search</button>
       <button>Clean</button>
       <div class="cardResult"></div>
