1b41d20105b2fc3c81888982ad95f5bf4f58726a
AndreCarpio <Andre.Carpio@jala.university>
2025-06-19 17:02:55 -0400

    checked function

--- src/components/molecules/Task.jsx
+++ src/components/molecules/Task.jsx
@@ -2,12 +2,31 @@ import React from "react";
 import { Button } from "../atoms/Button";
 import "./Task.css";
 
-export const Task = ({ id, name, completed }) => {
+export const Task = ({
+  id,
+  name,
+  completed = false,
+  deleteTask = () => {},
+  checkTask = () => {},
+}) => {
   return (
     <div className="task">
+      <input
+        type="checkbox"
+        checked={completed}
+        onChange={(e) => {
+          checkTask(e.target.checked, id);
+        }}
+      />
       <p className="taskName">{name}</p>
       <Button>Edit</Button>
-      <Button>Delete</Button>
+      <Button
+        onClick={() => {
+          deleteTask(id);
+        }}
+      >
+        Delete
+      </Button>
     </div>
   );
 };
--- src/components/organims/ToDoApp.jsx
+++ src/components/organims/ToDoApp.jsx
@@ -11,28 +11,59 @@ export const ToDoApp = () => {
     {
       id: "s21321",
       name: "Estudiar javascript",
-      completed: true,
+      completed: false,
     },
   ]);
+
+  const addTask = () => {
+    if (input.current.value != "") {
+      setTasks([
+        ...tasks,
+        {
+          id: crypto.randomUUID(),
+          name: input.current.value,
+          completed: false,
+        },
+      ]);
+    } else {
+      alert("Introduce a task name");
+    }
+  };
+
+  const checkTask = (completed, id) => {
+    console.log(completed);
+    setTasks((prev) => {
+      return prev.map((task) => {
+        if (task.id == id) {
+          task.completed = completed;
+        }
+        return task;
+      });
+    });
+  };
+
+  const deleteTask = (id) => {
+    setTasks((prev) => {
+      return prev.filter((task) => task.id != id);
+    });
+  };
+
   return (
     <>
       <div className="toDoApp">
         <Title>ToDo List App</Title>
         <InputText ref={input} placeholder={"Task name"}></InputText>
-        <Button
-          onClick={() => {
-            console.log(input.current.value);
-          }}
-        >
-          Add
-        </Button>
+        <Button onClick={addTask}>Add</Button>
 
         {tasks.map((task) => {
           return (
             <Task
               key={task.id}
+              id={task.id}
               name={task.name}
               completed={task.completed}
+              deleteTask={deleteTask}
+              checkTask={checkTask}
             ></Task>
           );
         })}
