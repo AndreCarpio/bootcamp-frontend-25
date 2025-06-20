e39c65eb3df9f114c8e044232b7d2e37d70f91be
AndreCarpio <Andre.Carpio@jala.university>
2025-06-20 17:07:00 -0400

    removing react strict mode

--- src/components/organims/ToDoApp.jsx
+++ src/components/organims/ToDoApp.jsx
@@ -1,4 +1,4 @@
-import { useReducer, useRef, useState } from "react";
+import { useReducer, useRef } from "react";
 import { InputText } from "../atoms/InputText";
 import { Title } from "../atoms/Title";
 import "./ToDoApp.css";
@@ -30,7 +30,7 @@ export const ToDoApp = () => {
       id,
     });
   };
-  
+
   const handleDeleteTask = (id) => {
     dispatch({
       type: "deleteTask",
