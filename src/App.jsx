d7d468e207500c731c34e216875519453e379ee4
AndreCarpio <Andre.Carpio@jala.university>
2025-06-23 16:37:31 -0400

    using context

---
+++ src/App.jsx
@@ -0,0 +1,60 @@
+import { useContext, useState } from "react";
+import { places } from "./data.js";
+import { getImageUrl } from "./utils.js";
+import { ImageContext } from "./Context.js";
+
+export default function App() {
+  const [isLarge, setIsLarge] = useState(false);
+  const imageSize = isLarge ? 150 : 100;
+  return (
+    <>
+      <ImageContext.Provider value={imageSize}>
+        <label>
+          <input
+            type="checkbox"
+            checked={isLarge}
+            onChange={(e) => {
+              setIsLarge(e.target.checked);
+            }}
+          />
+          Use large images
+        </label>
+        <hr />
+        <List imageSize={imageSize} />
+      </ImageContext.Provider>
+    </>
+  );
+}
+
+function List({ imageSize }) {
+  const listItems = places.map((place) => (
+    <li key={place.id}>
+      <Place place={place} imageSize={imageSize} />
+    </li>
+  ));
+  return <ul>{listItems}</ul>;
+}
+
+function Place({ place, imageSize }) {
+  return (
+    <>
+      <PlaceImage place={place} imageSize={imageSize} />
+      <p>
+        <b>{place.name}</b>
+        {": " + place.description}
+      </p>
+    </>
+  );
+}
+
+function PlaceImage({ place }) {
+  const imageSize = useContext(ImageContext);
+  return (
+    <img
+      src={getImageUrl(place)}
+      alt={place.name}
+      width={imageSize}
+      height={imageSize}
+    />
+  );
+}
