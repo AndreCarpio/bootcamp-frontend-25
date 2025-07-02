fdee2e9af3cb44ddaedc15606dd6ff3353d444cd
AndreCarpio <Andre.Carpio@jala.university>
2025-07-02 17:11:38 -0400

    changing time format

--- src/App.jsx
+++ src/App.jsx
@@ -41,9 +41,11 @@ export function App() {
     const totalSeconds = Math.floor(ms / 1000);
     const minutes = Math.floor(totalSeconds / 60);
     const seconds = totalSeconds % 60;
-    const paddedMinutes = String(minutes).padStart(2, "0");
-    const paddedSeconds = String(seconds).padStart(2, "0");
-    return `${paddedMinutes}:${paddedSeconds}`;
+
+    return (
+      `${minutes} ${minutes === 1 ? "minute" : "minutes"}, ` +
+      `${seconds} ${seconds === 1 ? "second" : "seconds"}  `
+    );
   }
 
   return (
